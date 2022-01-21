6af6261d21790804563b42f3d31daca0429a7307
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2022-01-21 11:55:35 -0600

    moved some code around and added a few items in the usestate list

--- src/App.js
+++ src/App.js
@@ -12,14 +12,23 @@ import NavBar from './Components/NavBar/NavBar';
 function App() {
 
   const [video, setVideo] = useState([])
-  const [videoId, setVideoId] = useState("Z83I2tz5UzM")
+  const [videoId, setVideoId] = useState(["Z83I2tz5UzM", "aS8L0hLOo4o", "YuhXi0aByn8", "BbUWsQ48T58"])
+  const [title, setTitle] = useState()
+  const [description, setDescription] = useState()
 
-
+  
 
   useEffect(() => {
     getVideo();
     getRelatedVideos();
+    getTitleDescription();
   }, [])
+  
+  async function getTitleDescription() {
+      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${videoId}}&key=${googleAPIKey}&part=snippet`)
+      setTitle(response.data.items[0].snippet.title)
+      setDescription(response.data.items[0].snippet.description)
+  }
 
   async function getRelatedVideos() {
     let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoId}&type=video&key=${googleAPIKey}&part=snippet`)
@@ -33,12 +42,13 @@ function App() {
     console.log(response.data.items)
   }
   console.log('video', video)
+  
   return (
     <div>
       <Router>
         <NavBar/>
         <Routes>
-          <Route path="/" element={<VideoPlayer someVideo={videoId} />}/>
+          <Route path="/" element={<VideoPlayer someVideo={videoId} title={title} description={description}/>}/>
           <Route path="/Login" element={<LoginForm />}/>
           <Route path="/register" element={<RegistrationForm />}/>
           <Route path="/Related" element={<RelatedVideos  video={video}/>}/>
--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,25 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { googleAPIKey } from '../../Keys';
+
 const VideoPlayer = (props) => {
-
-    const [title, setTitle] = useState()
-    const [description, setDescription] = useState()
-
-    useEffect(() => {
-        getTitleDescription();
-      }, [])
-
-    async function getTitleDescription() {
-        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${videoId}}&key=${googleAPIKey}&part=snippet,statistics`)
-        setTitle(response.data.items[0].snippet.title)
-        setDescription(response.data.items[0].snippet.description)
-    }
 
     return ( 
         <div>
-            <div><iframe id="ytplayer" type="text/html" width="640" height="360" src={`https://www.youtube.com/embed/${props.someVideo}?autoplay=1&origin=http://example.com`}frameBorder="0"></iframe>
-            <h4>{props.someVideo.title}</h4></div>
-            
+            <div><iframe id="ytplayer" type="text/html" width="640" height="360" src={`https://www.youtube.com/embed/${props.someVideo[0]}?autoplay=1&origin=http://example.com`}frameBorder="0"></iframe><h4>{props.someVideo.title}</h4></div>
+            <div><iframe id="ytplayer" type="text/html" width="640" height="360" src={`https://www.youtube.com/embed/${props.someVideo[1]}?autoplay=1&origin=http://example.com`}frameBorder="0"></iframe><h4>{props.someVideo.title}</h4></div>
+            <div><iframe id="ytplayer" type="text/html" width="640" height="360" src={`https://www.youtube.com/embed/${props.someVideo[2]}?autoplay=1&origin=http://example.com`}frameBorder="0"></iframe><h4>{props.someVideo.title}</h4></div>
+            <div><iframe id="ytplayer" type="text/html" width="640" height="360" src={`https://www.youtube.com/embed/${props.someVideo[3]}?autoplay=1&origin=http://example.com`}frameBorder="0"></iframe><h4>{props.someVideo.title}</h4></div>
         </div>
      );
 }
