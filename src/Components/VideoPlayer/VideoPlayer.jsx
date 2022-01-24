40f92ac35a63c8dd37d68c40790f46e266fe142a
CodyLuckmann <cluckmann@live.com>
2022-01-24 11:53:31 -0600

    added thumnails for searched videos

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -10,7 +10,7 @@ const VideoPlayer = (props) => {
             {/* <div><iframe id="ytplayer" type="text/html" width="640" height="360" src={`https://www.youtube.com/embed/${props.someVideo[1]}?autoplay=1&origin=http://example.com`}frameBorder="0"></iframe><h4>{props.someVideo.title}</h4></div>
             <div><iframe id="ytplayer" type="text/html" width="640" height="360" src={`https://www.youtube.com/embed/${props.someVideo[2]}?autoplay=1&origin=http://example.com`}frameBorder="0"></iframe><h4>{props.someVideo.title}</h4></div>
             <div><iframe id="ytplayer" type="text/html" width="640" height="360" src={`https://www.youtube.com/embed/${props.someVideo[3]}?autoplay=1&origin=http://example.com`}frameBorder="0"></iframe><h4>{props.someVideo.title}</h4></div> */}
-           {/* {props.searchResults.map((element)=> <img src={element.})} */}
+           {props.searchResults.map((element)=> <img src={element.snippet.thumbnails.default.url}></img>)} 
            <h1>{props.title}</h1>
         </div>
      );
