4b9bf034b76f0c8cf6af39ee14adf8401e95299b
CodyLuckmann <cluckmann@live.com>
2022-01-25 15:09:28 -0600

    Fixed an import

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { googleAPIKey } from '../../Keys';
+import { googleAPIKey } from '../../NewKeys';
 
 const VideoPlayer = (props) => {
 
