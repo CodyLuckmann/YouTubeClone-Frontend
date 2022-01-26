bf9437a946b533f2f84a9e46665c1ccbec46f7e1
CodyLuckmann <cluckmann@live.com>
2022-01-26 10:14:01 -0600

    changes to comment form

--- src/Components/CommentForm/CommentForm.jsx
+++ src/Components/CommentForm/CommentForm.jsx
@@ -4,7 +4,7 @@ import { React, useState } from 'react';
 
 const CommentForm = (props) => {
 
-    const [video_id, setVideoId] = useState('');
+    
     const [text, setText] = useState('');
     const jwt = localStorage.getItem('token')
 
@@ -14,21 +14,22 @@ const CommentForm = (props) => {
         e.preventDefault();
         let comment = 
             {
-                video_id: video_id,
+                
                 text: text,
 
             }
-        let response = await axios.post('http://127.0.0.1:8000/api/comments/', {headers: {Authorization: 'Bearer ' + jwt}}, comment);
+        console.log(comment)
+        let response = await axios.post('http://127.0.0.1:8000/api/comments/', comment ,{headers: {Authorization: 'Bearer ' + jwt}});
         props.addComment(response);
         
     }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Comment</label>
                 <input type='text' value={text} onChange={(event) => setText(event.target.value)}/>
-                <button onClick={handleSubmit} className="btn" type="submit">
+                <button className="btn" type="submit">
               Submit
             </button>
             </div>
